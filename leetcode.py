202fdc12fcce44361ebf45b10d6047f197f35ec4
Pan <68371938+Eyepan@users.noreply.github.com>
2023-03-10 23:42:27 +0530

    pivot success

--- leetcode.py
+++ leetcode.py
@@ -2,29 +2,27 @@ import json
 import requests
 import math
 import sqlite3
-import asyncio
+import threading
 import pandas as pd
-import aiohttp
-import sys
+import queue
 from tqdm import tqdm
 
 
-async def make_request(contest_code, i, pbar):
+def make_request(contest_code, i, pbar, results_queue):
     url = f"https://leetcode.com/contest/api/ranking/{contest_code}/"
     params = {
         "pagination": i + 1,
     }
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as response:
-            if response.status != 200:
-                print(f"Response failed while requesting page {i + 1}")
-                return 0
-            data = (await response.json())['total_rank']
-            pbar.update(1)
-            return data
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        print(f"Response failed while requesting page {i + 1}")
+        exit()
+    data = response.json()['total_rank']
+    results_queue.put(data)
+    pbar.update(1)
 
 
-async def glc(contest_code: str):
+def glc(contest_code: str):
     url = f"https://leetcode.com/contest/api/ranking/{contest_code}/"
     params = {
         "pagination": 1,
@@ -32,40 +30,41 @@ async def glc(contest_code: str):
     print(f"Fetching data for {contest_code}")
     response = requests.get(url, params=params)
 
-    if (response.status_code != 200):
+    if response.status_code != 200:
         print("something went wrong")
-        raise 'status code not 200'
+        exit()
 
     total_requests_to_make = math.ceil(
         response.json()['user_num'] / len(response.json()['total_rank']))
 
-    num_threads = 4
     total_ranks = []
 
     print(
         f"Getting {total_requests_to_make} pages of data for {contest_code} with {response.json()['user_num']} participants")
 
     with tqdm(total=total_requests_to_make) as pbar:
-        tasks = []
+        results_queue = queue.Queue()
+        threads = []
         for i in range(total_requests_to_make):
-            tasks.append(asyncio.ensure_future(
-                make_request(contest_code, i, pbar)))
+            thread = threading.Thread(target=make_request, args=(
+                contest_code, i, pbar, results_queue))
+            threads.append(thread)
+            thread.start()
 
-        results = await asyncio.gather(*tasks)
+        for thread in threads:
+            thread.join()
 
-    total_ranks = sum(results, [])
+        while not results_queue.empty():
+            total_ranks.extend(results_queue.get())
 
     df = pd.read_json(json.dumps(total_ranks))
 
     df = df[['username', 'rank', 'score', 'finish_time']]
     df.to_sql(f'leetcode-{contest_code}', con=sqlite3.connect(
         "db.sqlite3"), if_exists="replace")
-
+    df.to_csv(f"leetcode_{contest_code}.csv", index=False)
     print(f"Done for {contest_code}!")
 
 
 def get_leetcode(contest_code: str):
-    asyncio.get_event_loop().run_until_complete(glc(contest_code))
-
-
-get_leetcode('weekly-contest-229')
+    glc(contest_code)
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import pandas as pd
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import sqlite3
+from leetcode import get_leetcode
+import nest_asyncio
+nest_asyncio.apply()
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount('/static', StaticFiles(directory='static'), name='static')
+conn = sqlite3.connect('db.sqlite3')
+cursor = conn.cursor()
+
+
+@app.on_event("startup")
+def startup():
+    # get_leetcode('weekly-contest-300')
+    pass
 
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+def index(request: Request):
+    result = pd.read_csv('leetcode_weekly-contest-300.csv').to_html()
+    return templates.TemplateResponse("index.html", {"request": request, "result": result})
