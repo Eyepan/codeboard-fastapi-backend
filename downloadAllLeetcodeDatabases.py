f49a1642f827c75286fa17a0efcadbf67ba73861
Pan <68371938+Eyepan@users.noreply.github.com>
2023-03-12 22:36:38 +0530

    prefetched

--- downloadAllLeetcodeDatabases.py
+++ downloadAllLeetcodeDatabases.py
@@ -1,12 +1,9 @@
-from time import sleep
 import requests
 
 
 url = 'http://localhost:8000/api/leetcode/contest/weekly-contest-'
 
-for i in range(275, 336):
+for i in range(287, 336):
     print("Fetching data for contest " + str(i))
     response = requests.get(url + str(i))
-    # sleep for an unsuspicious amount of time
-    sleep(0.5)
     print(response.status_code, response.json())
