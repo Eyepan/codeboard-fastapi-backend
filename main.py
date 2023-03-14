797d9a3adcc29e71d8c9312ed7ace4e1e433f7be
Pan <68371938+Eyepan@users.noreply.github.com>
2023-03-14 15:06:39 +0530

    one stupid bug later

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.database import initDB
-from src.routes.routes_codechef import router as codechef_router
-from src.routes.routes_leetcode import router as leetcode_router
+from src.database import students_db, init_students_db
 from src.routes.routes_students import router as students_router
+from src.routes.routes_leetcode import router as leetcode_router
+from src.routes.routes_codechef import router as codechef_router
 
 app = FastAPI()
 
-origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(students_router)
 app.include_router(leetcode_router)
@@ -25,9 +25,9 @@ app.include_router(codechef_router)
 
 @app.on_event("startup")
 async def startup():
-    initDB()
+    init_students_db()
 
 
 @app.get("/")
-async def hello():
-    return "hi there"
+async def get_index():
+    return "hello there"
